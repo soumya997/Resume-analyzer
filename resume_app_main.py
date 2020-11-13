e1ae4310d45330eb558be99f4a76465b4f59a586
soumya997 <soumya997.sarkar@gmail.com>
2020-11-13 14:46:59 +0530

    adding ascii

--- resume_app_main.py
+++ resume_app_main.py
@@ -17,6 +17,19 @@ import logging
 from werkzeug.debug import DebuggedApplication  
 
 
+"""
+  ______  _              _     __          __    _                           
+ |  ____|| |            | |    \ \        / /   | |                          
+ | |__   | |  __ _  ___ | | __  \ \  /\  / /___ | |__     __ _  _ __   _ __  
+ |  __|  | | / _` |/ __|| |/ /   \ \/  \/ // _ \| '_ \   / _` || '_ \ | '_ \ 
+ | |     | || (_| |\__ \|   <     \  /\  /|  __/| |_) | | (_| || |_) || |_) |
+ |_|     |_| \__,_||___/|_|\_\     \/  \/  \___||_.__/   \__,_|| .__/ | .__/ 
+                                                               | |    | |    
+                                 
+
+"""
+
+
 # logging.basicConfig(filename='resume-log.log',level=logging.DEBUG,
                     # format='%(Lavelname)s:%(filename)s:%(funcName)s:%(massage)s')
 
